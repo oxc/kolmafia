oxc/kolmafia

commit f30bbad70b27f4784cf4afd71f2d5d63210b7323

Fix problem with incorrect HP target being shown for Basement gauntlet test, if your DA is over 1000.
Fix problem with Basement monsters being misrecognized as hobos if your last
normal adventure was in Hobopolis.


git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@6493 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/KoLAdventure.java
@@ -1029,7 +1029,8 @@ public class KoLAdventure
 
 		// Not an internal location.  Perhaps it's something related
 		// to another common request?
-
+		
+		KoLAdventure.lastVisitedLocation = null;
 		String location = AdventureDatabase.getUnknownName( urlString );
 		if ( location == null )
 		{

src/net/sourceforge/kolmafia/request/BasementRequest.java
@@ -849,12 +849,12 @@ public class BasementRequest
 				if ( autoSwitch )
 				{
 					BasementRequest.changeBasementOutfit( "gauntlet" );
-				}
 
-				damageAbsorb =
-					1.0f - ( (float) Math.sqrt( KoLCharacter.getDamageAbsorption() / 10.0f ) - 1.0f ) / 10.0f;
-				healthRequirement = drainRequirement * damageAbsorb;
-				BasementRequest.basementTestValue = (int) healthRequirement;
+					damageAbsorb =
+						1.0f - ( (float) Math.sqrt( Math.min( 1000, KoLCharacter.getDamageAbsorption() ) / 10.0f ) - 1.0f ) / 10.0f;
+					healthRequirement = drainRequirement * damageAbsorb;
+					BasementRequest.basementTestValue = (int) healthRequirement;
+				}
 
 				if ( KoLCharacter.getMaximumHP() < healthRequirement )
 				{

commit 12a3674105869d696804f433c152f395b8396651

Fix problem introduced in r6493 that was causing a variety of location-specific actions to not take place.
git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@6494 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/KoLAdventure.java
@@ -1030,7 +1030,6 @@ public class KoLAdventure
 		// Not an internal location.  Perhaps it's something related
 		// to another common request?
 		
-		KoLAdventure.lastVisitedLocation = null;
 		String location = AdventureDatabase.getUnknownName( urlString );
 		if ( location == null )
 		{
@@ -1052,6 +1051,7 @@ public class KoLAdventure
 
 		if ( shouldReset )
 		{
+			KoLAdventure.lastVisitedLocation = null;
 			KoLAdventure.resetAutoAttack();
 		}
 