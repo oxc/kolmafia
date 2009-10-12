oxc/kolmafia

commit d8f1a4d20a06a32e4a082637abbbc9d235ef7f7d

Fix parsing of Malus use with the "as many as possible" option checked.
Yet another time
Haiku Dungeon parsing fixed
Here, have a cheeto

Start of Player Status frame revamp - remove the redundant display of your
current stats, leaving just the till-next-point data which isn't shown
anywhere else.


git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@7729 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/request/CreateItemRequest.java
@@ -679,7 +679,7 @@ public class CreateItemRequest
 		}
 
 		AdventureResult [] ingredients = CreateItemRequest.findIngredients( urlString );
-		int quantity = CreateItemRequest.getQuantity( urlString, ingredients ) * multiplier;
+		int quantity = CreateItemRequest.getQuantity( urlString, ingredients, multiplier );
 
 		for ( int i = 0; i < ingredients.length; ++i )
 		{
@@ -1207,7 +1207,7 @@ public class CreateItemRequest
 
 		AdventureResult [] ingredients = CreateItemRequest.findIngredients( urlString );
 
-		int quantity = CreateItemRequest.getQuantity( urlString, ingredients ) * multiplier;
+		int quantity = CreateItemRequest.getQuantity( urlString, ingredients, multiplier );
 
 		for ( int i = 0; i < ingredients.length; ++i )
 		{
@@ -1290,9 +1290,10 @@ public class CreateItemRequest
 		return ItemPool.get( StringUtilities.parseInt( itemId ), 1 );
 	}
 
-	private static final int getQuantity(  final String urlString, final AdventureResult [] ingredients )
+	private static final int getQuantity( final String urlString, final AdventureResult [] ingredients, int multiplier )
 	{
-		if ( urlString.indexOf( "max=on" ) == -1 )
+		if ( urlString.indexOf( "max=on" ) == -1 &&
+			urlString.indexOf( "smashall=1" ) == -1 )
 		{
 			Matcher matcher = CreateItemRequest.QUANTITY_PATTERN.matcher( urlString );
 			return matcher.find() ? StringUtilities.parseInt( matcher.group( 2 ) ) : 1;
@@ -1303,10 +1304,10 @@ public class CreateItemRequest
 		for ( int i = 0; i < ingredients.length; ++i )
 		{
 			AdventureResult item = ingredients[i];
-			quantity = Math.min( item.getCount( KoLConstants.inventory ), quantity );
+			quantity = Math.min( item.getCount( KoLConstants.inventory ) / multiplier, quantity );
 		}
 
-		return quantity;
+		return quantity * multiplier;
 	}
 
 	private static final void useIngredients( final String urlString, AdventureResult [] ingredients, int quantity )

src/net/sourceforge/kolmafia/session/ResultProcessor.java
@@ -71,7 +71,7 @@ public class ResultProcessor
 	private static Pattern CARBS_PATTERN =
 		Pattern.compile( "some of your blood, to the tune of ([\\d,]+) damage" );
 	private static Pattern DISCARD_PATTERN = Pattern.compile( "You discard your (.*?)\\." );
-	private static Pattern HAIKU_PATTERN = Pattern.compile( "<[tT]able><tr><td[^>]*?><img[^>]*/([^/]*\\.gif)[^>]*?('descitem\\((.*?)\\)')?></td>(<td[^>]*><[tT]able><tr>)?<td[^>]*?>(.*?)</td>(</tr></table></td>)?</tr></table>" );
+	private static Pattern HAIKU_PATTERN = Pattern.compile( "<[tT]able[^>]*?><tr><td[^>]*?><img[^>]*/([^/]*\\.gif)[^>]*?('descitem\\((.*?)\\)')?></td>(<td[^>]*><[tT]able><tr>)?<td[^>]*?>(.*?)</td>(</tr></table></td>)?</tr></table>" );
 	private static Pattern INT_PATTERN = Pattern.compile( ".*?([\\d]+).*" );
 
 	private static AdventureResult haikuEffect = EffectPool.get( EffectPool.HAIKU_STATE_OF_MIND );

src/net/sourceforge/kolmafia/swingui/CharSheetFrame.java
@@ -38,6 +38,7 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -53,7 +54,6 @@ public class CharSheetFrame
 	extends GenericFrame
 {
 	private final JLabel avatar;
-	private JLabel[] statusLabel;
 	private JProgressBar[] tnpDisplay;
 	private final KoLCharacterAdapter statusRefresher;
 
@@ -73,14 +73,12 @@ public class CharSheetFrame
 
 		statusPanel.add( this.createStatusPanel(), BorderLayout.CENTER );
 		statusPanel.add( this.avatar, BorderLayout.WEST );
-
+		
 		JPanel statusContainer = new JPanel( new CardLayout( 10, 10 ) );
 		statusContainer.add( statusPanel, "" );
 
-		JPanel northPanel = new JPanel( new BorderLayout() );
-		northPanel.add( statusContainer, BorderLayout.WEST );
-
-		this.framePanel.add( northPanel, BorderLayout.NORTH );
+		this.framePanel.add( statusContainer, BorderLayout.NORTH );
+		//this.framePanel.add( new JLabel( "summary panel goes here" ), BorderLayout.CENTER );
 
 		this.statusRefresher = new KoLCharacterAdapter( new StatusRefreshRunnable() );
 		KoLCharacter.addCharacterListener( this.statusRefresher );
@@ -94,51 +92,18 @@ public class CharSheetFrame
 	}
 
 	/**
-	 * Utility method for creating a panel that displays the given label, using formatting if the values are different.
-	 */
-
-	private JPanel createValuePanel( final String title, final int displayIndex )
-	{
-		int index1 = 2 * displayIndex;
-		int index2 = index1 + 1;
-
-		this.statusLabel[ index1 ] = new JLabel( " ", SwingConstants.LEFT );
-		this.statusLabel[ index1 ].setForeground( Color.BLUE );
-		this.statusLabel[ index2 ] = new JLabel( " ", SwingConstants.LEFT );
-
-		JPanel headerPanel = new JPanel();
-		headerPanel.setLayout( new BoxLayout( headerPanel, BoxLayout.X_AXIS ) );
-
-		headerPanel.add( new JLabel( title + ":  ", SwingConstants.RIGHT ) );
-		headerPanel.add( this.statusLabel[ index1 ] );
-		headerPanel.add( this.statusLabel[ index2 ] );
-
-		JPanel valuePanel = new JPanel( new BorderLayout( 2, 2 ) );
-		valuePanel.add( headerPanel, BorderLayout.EAST );
-		valuePanel.add( this.tnpDisplay[ displayIndex ], BorderLayout.SOUTH );
-
-		return valuePanel;
-	}
-
-	/**
 	 * Utility method for modifying a panel that displays the given label, using formatting if the values are different.
 	 */
 
-	private void refreshValuePanel( final int displayIndex, final int baseValue, final int adjustedValue,
-		final int tillNextPoint )
+	private void refreshValuePanel( final int displayIndex, final int baseValue,
+		final int tillNextPoint, final String label )
 	{
-		int index1 = 2 * displayIndex;
-		int index2 = index1 + 1;
-
-		JLabel adjustedLabel = this.statusLabel[ index1 ];
-		JLabel baseLabel = this.statusLabel[ index2 ];
-
-		adjustedLabel.setText( KoLConstants.COMMA_FORMAT.format( adjustedValue ) );
-		baseLabel.setText( " (" + KoLConstants.COMMA_FORMAT.format( baseValue ) + ")" );
-
-		this.tnpDisplay[ displayIndex ].setMaximum( 2 * baseValue + 1 );
-		this.tnpDisplay[ displayIndex ].setValue( 2 * baseValue + 1 - tillNextPoint );
-		this.tnpDisplay[ displayIndex ].setString( KoLConstants.COMMA_FORMAT.format( this.tnpDisplay[ displayIndex ].getValue() ) + " / " + KoLConstants.COMMA_FORMAT.format( this.tnpDisplay[ displayIndex ].getMaximum() ) );
+		JProgressBar tnp = this.tnpDisplay[ displayIndex ];
+		tnp.setMaximum( 2 * baseValue + 1 );
+		tnp.setValue( 2 * baseValue + 1 - tillNextPoint );
+		tnp.setString( label +
+			KoLConstants.COMMA_FORMAT.format( tnp.getValue() ) + " / " +
+			KoLConstants.COMMA_FORMAT.format( tnp.getMaximum() ) );
 	}
 
 	/**
@@ -148,16 +113,10 @@ public class CharSheetFrame
 	 * @return a <code>JPanel</code> displaying the character's statistics
 	 */
 
-	private JPanel createStatusPanel()
+	private Box createStatusPanel()
 	{
-		JPanel statusLabelPanel = new JPanel();
-		statusLabelPanel.setLayout( new BoxLayout( statusLabelPanel, BoxLayout.Y_AXIS ) );
-
-		this.statusLabel = new JLabel[ 6 ];
-		for ( int i = 0; i < 6; ++i )
-		{
-			this.statusLabel[ i ] = new JLabel( " ", SwingConstants.CENTER );
-		}
+		Box statusPanel = Box.createVerticalBox();
+		statusPanel.add( Box.createVerticalGlue() );
 
 		this.tnpDisplay = new JProgressBar[ 3 ];
 		for ( int i = 0; i < 3; ++i )
@@ -165,15 +124,11 @@ public class CharSheetFrame
 			this.tnpDisplay[ i ] = new JProgressBar();
 			this.tnpDisplay[ i ].setValue( 0 );
 			this.tnpDisplay[ i ].setStringPainted( true );
+			statusPanel.add( this.tnpDisplay[ i ] );
+			statusPanel.add( Box.createVerticalGlue() );
 		}
 
-		JPanel primeStatPanel = new JPanel( new GridLayout( 3, 1, 5, 5 ) );
-		primeStatPanel.add( this.createValuePanel( "Muscle", 0 ) );
-		primeStatPanel.add( this.createValuePanel( "Mysticality", 1 ) );
-		primeStatPanel.add( this.createValuePanel( "Moxie", 2 ) );
-		statusLabelPanel.add( primeStatPanel );
-
-		return statusLabelPanel;
+		return statusPanel;
 	}
 
 	private class StatusRefreshRunnable
@@ -182,12 +137,13 @@ public class CharSheetFrame
 		public void run()
 		{
 			CharSheetFrame.this.refreshValuePanel(
-				0, KoLCharacter.getBaseMuscle(), KoLCharacter.getAdjustedMuscle(), KoLCharacter.getMuscleTNP() );
+				0, KoLCharacter.getBaseMuscle(),
+				KoLCharacter.getMuscleTNP(), "Mus: " );
 			CharSheetFrame.this.refreshValuePanel(
-				1, KoLCharacter.getBaseMysticality(), KoLCharacter.getAdjustedMysticality(),
-				KoLCharacter.getMysticalityTNP() );
+				1, KoLCharacter.getBaseMysticality(), KoLCharacter.getMysticalityTNP(), "Mys: " );
 			CharSheetFrame.this.refreshValuePanel(
-				2, KoLCharacter.getBaseMoxie(), KoLCharacter.getAdjustedMoxie(), KoLCharacter.getMoxieTNP() );
+				2, KoLCharacter.getBaseMoxie(),
+				KoLCharacter.getMoxieTNP(), "Mox: " );
 
 			// Set the current avatar
 			CharSheetFrame.this.avatar.setIcon( JComponentUtilities.getImage( KoLCharacter.getAvatar() ) );

commit 0ae9ded73aa5fa1a9e207da3afb71233faa1630d

Fix problem with Malus use other than "as many as possible", introduced in r7729.
git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@7732 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/request/CreateItemRequest.java
@@ -1296,7 +1296,9 @@ public class CreateItemRequest
 			urlString.indexOf( "smashall=1" ) == -1 )
 		{
 			Matcher matcher = CreateItemRequest.QUANTITY_PATTERN.matcher( urlString );
-			return matcher.find() ? StringUtilities.parseInt( matcher.group( 2 ) ) : 1;
+			return matcher.find() ?
+				StringUtilities.parseInt( matcher.group( 2 ) ) * multiplier :
+				multiplier;
 		}
 
 		int quantity = Integer.MAX_VALUE;