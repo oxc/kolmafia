a1c9ac0b21bc42584c6e349d2ac8adb3b75c0d30
Bernhard Frauendienst <git@nospam.obeliks.de>
2021-10-15 00:28:20 +0200

    oxc: add buffoonery client

--- src/net/sourceforge/kolmafia/textui/RuntimeLibrary.java
+++ src/net/sourceforge/kolmafia/textui/RuntimeLibrary.java
@@ -84,6 +84,7 @@ import net.sourceforge.kolmafia.objectpool.ConcoctionPool;
 import net.sourceforge.kolmafia.objectpool.EffectPool;
 import net.sourceforge.kolmafia.objectpool.FamiliarPool;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.oxc.BuffooneryHttpClient;
 import net.sourceforge.kolmafia.persistence.AdventureDatabase;
 import net.sourceforge.kolmafia.persistence.CandyDatabase;
 import net.sourceforge.kolmafia.persistence.CandyDatabase.Candy;
@@ -192,6 +193,12 @@ import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 @SuppressWarnings("unused")
 public abstract class RuntimeLibrary {
+  private static final RecordType buffooneryResponseRec =
+      new RecordType(
+          "{int code; string body;}",
+          new String[] {"code", "body"},
+          new Type[] {DataTypes.INT_TYPE, DataTypes.STRING_TYPE});
+
   private static final RecordType channelMessageRec =
       new RecordType(
           "{string event; string params;}",
@@ -432,6 +439,22 @@ public abstract class RuntimeLibrary {
             "form_fields",
             new AggregateType(DataTypes.STRING_TYPE, DataTypes.STRING_TYPE),
             params));
+
+    params =
+        new Type[] {
+          DataTypes.STRING_TYPE, DataTypes.STRING_TYPE, DataTypes.STRING_TYPE, DataTypes.INT_TYPE
+        };
+    functions.add(new LibraryFunction("buffoonery_make_request", DataTypes.AGGREGATE_TYPE, params));
+
+    params =
+        new Type[] {
+          DataTypes.STRING_TYPE,
+          DataTypes.STRING_TYPE,
+          DataTypes.STRING_TYPE,
+          DataTypes.STRING_TYPE,
+          DataTypes.INT_TYPE
+        };
+    functions.add(new LibraryFunction("buffoonery_make_request", DataTypes.AGGREGATE_TYPE, params));
 
 
     params = new Type[] {DataTypes.STRING_TYPE, DataTypes.STRING_TYPE};
@@ -3298,6 +3321,41 @@ public abstract class RuntimeLibrary {
     }
 
     return value;
+  }
+
+  public static Value buffoonery_make_request(
+      ScriptRuntime controller,
+      final Value httpMethod,
+      final Value httpPath,
+      final Value query,
+      final Value timeout) {
+    return buffoonery_make_request(controller, httpMethod, httpPath, query, new Value(""), timeout);
+  }
+
+  public static Value buffoonery_make_request(
+      ScriptRuntime controller,
+      final Value httpMethod,
+      final Value httpPath,
+      final Value query,
+      final Value body,
+      final Value timeout) {
+    try {
+      var response =
+          BuffooneryHttpClient.INSTANCE.makeRequest(
+              httpMethod.contentString,
+              httpPath.contentString,
+              query.contentString,
+              body.contentString,
+              timeout.intValue());
+
+      var result = new RecordValue(buffooneryResponseRec);
+      result.aset(0, DataTypes.makeIntValue(response.getCode()), null);
+      result.aset(1, DataTypes.makeStringValue(response.getBody()), null);
+
+      return result;
+    } catch (Exception e) {
+      throw controller.runtimeException2("Error making buffoonery REST call", e.getMessage());
+    }
   }
 
   public static Value spawn_thread(
