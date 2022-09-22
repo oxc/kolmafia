6cff254c59125a37bc056200e98c602c38a9b139
Bernhard Frauendienst <git@nospam.obeliks.de>
2022-09-22 10:59:35 +0200

    fix: move Option out of ChoiceAdventures to fix circular dependency
    https://kolmafia.us/threads/vampout-is-broken-due-to-circular-dependency.28069/

--- src/net/sourceforge/kolmafia/session/WumpusManager.java
+++ src/net/sourceforge/kolmafia/session/WumpusManager.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 import net.sourceforge.kolmafia.KoLConstants.MafiaState;
 import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.RequestLogger;
-import net.sourceforge.kolmafia.session.ChoiceAdventures.Option;
 import net.sourceforge.kolmafia.webui.RelayLoader;
 
 public abstract class WumpusManager {
