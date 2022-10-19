7c3653495c47adb56d04ca3540310a463d8612f5
Bernhard Frauendienst <git@nospam.obeliks.de>
2022-10-20 01:56:09 +0200

    feat: add thread and message passing support for scripts
    * Add a new spawn_thread function that can start another script file in a
      different thread
    * Add new post_message / poll_message functions that allow passing
      messages between threads and waiting for messages

---
+++ src/net/sourceforge/kolmafia/textui/MessageChannel.java
@@ -0,0 +1,47 @@
+package net.sourceforge.kolmafia.textui;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.Nullable;
+
+public class MessageChannel {
+
+  public static class Message {
+    public final String event;
+
+    public final @Nullable String params;
+
+    public Message(String event, @Nullable String params) {
+      this.event = event;
+      this.params = params;
+    }
+  }
+
+  private static final Map<String, MessageChannel> channels = new ConcurrentHashMap<>();
+
+  public static MessageChannel getChannel(final String name) {
+    return channels.computeIfAbsent(name, k -> new MessageChannel(name));
+  }
+
+  private final LinkedTransferQueue<Message> queue = new LinkedTransferQueue<>();
+
+  private final String channelName;
+
+  private MessageChannel(String channelName) {
+    this.channelName = channelName;
+  }
+
+  public void postMessage(Message message) {
+    queue.add(message);
+  }
+
+  public @Nullable Message pollMessage() {
+    return queue.poll();
+  }
+
+  public @Nullable Message pollMessage(long timeout, TimeUnit unit) throws InterruptedException {
+    return queue.poll(timeout, unit);
+  }
+}
